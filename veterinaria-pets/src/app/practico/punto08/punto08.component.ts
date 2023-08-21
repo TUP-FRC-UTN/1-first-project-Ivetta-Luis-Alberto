fa6b36be54713da078a2deab64813254689d1de1
luis ivetta <114588@tecnicatura.frc.utn.edu.ar>
2023-08-21 11:14:18 -0300

    punto 8

--- veterinaria-pets/src/app/practico/punto08/punto08.component.ts
+++ veterinaria-pets/src/app/practico/punto08/punto08.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Punto08Component implements OnInit {
 
+
+
+  coleccionMascotas: IMascota [] = [
+    {
+    nombre: "panchito",
+    raza:  "calle",
+    sexo:  "machito",
+    edad:  5,
+    nacimiento:  2017,
+    tutor:  "juanito",
+  },
+  {
+    nombre: "manchita",
+    raza:  "calle",
+    sexo:  "hembrita",
+    edad:  2,
+    nacimiento:  2021,
+    tutor:  "pedrito",
+  },
+  {
+    nombre: "pirata", 
+    raza:  "calle",
+    sexo:  "machito",
+    edad:  1,
+    nacimiento:  2022,
+    tutor:  "carlitos",  
+}]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface IMascota {
+  nombre: String,
+  raza: String,
+  sexo: String,
+  edad: Number,
+  nacimiento: Number,
+  tutor: String,
 }
